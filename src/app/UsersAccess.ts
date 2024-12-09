3e2d3d60f6f936e35ba58d840570923eb7f3285b
Stephen Moon <stephen.d.moon@gmail.com>
2024-12-09 16:33:26 -0500

    Local DB testing with Docker

---
+++ src/app/UsersAccess.ts
@@ -0,0 +1,27 @@
+import { User } from "./UserModel";
+import * as dbConnection from "./DatabaseConnector";
+
+export class UsersAccess {
+  public async connectToDb() {
+    await dbConnection.connect();
+  }
+
+  public async closeConnection() {
+    await dbConnection.close();
+  }
+
+  public async addUser(user: User) {
+    const id = await dbConnection.addUser(user);
+    return id;
+  }
+
+  public async getUser(id: string) {
+    const result = await dbConnection.getUser(id);
+    return result;
+  }
+
+  public async getAllUsers() {
+    const result = await dbConnection.getAllUsers();
+    return result;
+  }
+}
---
+++ src/test/UsersAccess.test.ts
@@ -0,0 +1,34 @@
+import { User } from "../app/UserModel";
+import { UsersAccess } from "../app/UsersAccess";
+
+describe("UsersAccess test suite", () => {
+  let usersAccess: UsersAccess;
+
+  const someUser: User = {
+    name: "Some User",
+    position: "Engineer",
+    employedAt: new Date(),
+  };
+
+  beforeAll(async () => {
+    usersAccess = new UsersAccess();
+    await usersAccess.connectToDb();
+  });
+
+  let userId: string;
+
+  test("insert a user", async () => {
+    userId = await usersAccess.addUser(someUser);
+    const a = 5;
+  });
+
+  test("find user", async () => {
+    const retrievedUser = await usersAccess.getUser(userId);
+    const a = 5;
+    expect(retrievedUser).toEqual(someUser);
+  });
+
+  afterAll(async () => {
+    await usersAccess.closeConnection();
+  });
+});
